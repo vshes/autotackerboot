2b05baa2707d7f031e3ace2f991b2e4f91e519df
vshes <vshes.60@gmail.com>
2017-07-04 18:14:35 -0500

    (chors) add production AWS configuration

--- src/main/java/io/shesh/tracker/Application.java
+++ src/main/java/io/shesh/tracker/Application.java
@@ -3,6 +3,7 @@ package io.shesh.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.AbstractEnvironment;
 
 /**
  * Created by shesh on 6/29/17.
@@ -12,6 +13,8 @@ import org.springframework.context.annotation.Import;
 public class Application {
 
     public static void main(String[] args) {
+
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME,"prod");
         SpringApplication.run(Application.class);
     }
 }
